6ed38c0a835533eabc4fb5c3c252834fd4c8a901
tom_glb <good_glb@mfg.com>
2018-11-20 10:50:59 +0400

    aadfadfa

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api':{
-        target: 'http://127.0.0.1:8001',
+        target: 'http://172.30.1.83:8001',
         changeOrigin: true,// 支持跨域
         pathRewrite: {
           '^/api': ''
